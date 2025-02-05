2d3978ad8aa6fde2b1197ced07396e29fa92689e
Muslim Mageza <muslimmageza@MacBook-Pro-Muslim.local>
2025-02-06 00:01:46 +0600

    add: token

--- src/ui/api.js
+++ src/ui/api.js
@@ -7,7 +7,7 @@ export const api = axios.create({
         'Accept': 'application/json',
         'Content-Type': 'application/json',
         'Accept-Language': '',
-        'Authorization': localStorage.getItem("MAZA-TOKEN")
+        'Authorization': "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzcwMzEyNTAxLCJpYXQiOjE3Mzg3NzY1MDEsImp0aSI6IjFiYmI1NDY0ODAzNTQwOTg5NDBmMWJiYTk0MjRiOWZhIiwidXNlcl9pZCI6OX0.YCi7OzlAIG5R_LIrbhmG5IQT5Vq7uMeqFZRxG-OVJws"
     }
 
 })
